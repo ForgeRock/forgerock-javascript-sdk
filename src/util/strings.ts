bffcffa9c3ce953c5909468d499daf0d0cd9c81a
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2019-10-17 16:05:19 -0500

    idm error handling detached from the api. includes tests

---
+++ src/util/strings.ts
@@ -0,0 +1,10 @@
+export function plural(
+    n: number,
+    singularText: string,
+    pluralText?: string,
+  ) {
+    if (n === 1) {
+      return singularText;
+    }
+    return pluralText !== undefined ? pluralText : singularText + 's';
+  }
