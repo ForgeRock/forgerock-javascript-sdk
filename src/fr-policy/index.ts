77c7ec60bb4de68c308a2c7489b87806b721ef1d
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2019-10-23 09:40:36 -0500

    moved unkown string to namespace enums

--- src/fr-policy/index.ts
+++ src/fr-policy/index.ts
@@ -1,43 +1,31 @@
-// {
-//   "code": 403,
-//   "reason": "Forbidden",
-//   "message": "Policy validation failed",
-//   "detail": {
-//     "result": false,
-//     "failedPolicyRequirements": [
-//       {
-//         "policyRequirements": [{ "policyRequirement": "UNIQUE" }],
-//         "property": "userName"
-//       }
-//     ]
-//   }
-// }
-
 import { Step } from 'auth/interfaces';
 import { FailedPolicyRequirement, PolicyRequirement } from '../auth/interfaces';
 import { PolicyMessage, PolicyName } from './enums';
 import { MessageCreator, ProcessedPropertyError } from './interfaces';
 
 abstract class FRPolicy {
-
   public static parseErrors(
-    err: Partial<Step>, customMessage?: MessageCreator,
+    err: Partial<Step>,
+    customMessage?: MessageCreator,
   ): ProcessedPropertyError[] {
     let errors: ProcessedPropertyError[] = [];
     if (err.detail && err.detail.failedPolicyRequirements) {
       errors = [];
       err.detail.failedPolicyRequirements.map((x) => {
-        errors.push.apply(errors, [{
-          detail: x,
-          messages: this.parseFailedPolicyRequirement(x, customMessage),
-        }]);
+        errors.push.apply(errors, [
+          {
+            detail: x,
+            messages: this.parseFailedPolicyRequirement(x, customMessage),
+          },
+        ]);
       });
     }
     return errors;
   }
 
   public static parseFailedPolicyRequirement(
-    failePolicy: FailedPolicyRequirement, customMessage?: MessageCreator,
+    failePolicy: FailedPolicyRequirement,
+    customMessage?: MessageCreator,
   ): string[] {
     const errors: string[] = [];
     failePolicy.policyRequirements.map((policyRequirement) => {
@@ -49,15 +37,17 @@ abstract class FRPolicy {
   }
 
   public static parsePolicyRequirement(
-    property: string, policy: PolicyRequirement, customMessage: MessageCreator = {},
+    property: string,
+    policy: PolicyRequirement,
+    customMessage: MessageCreator = {},
   ): string {
-
-    const policyName: { [key: string]: string; } = PolicyName;
-    const policyMessage: MessageCreator  = { ...PolicyMessage, ...customMessage };
-    let message = `${property}: Unknown policy requirement "${policy.policyRequirement}"`;
+    const policyName: { [key: string]: string } = PolicyName;
+    const policyMessage: MessageCreator = { ...PolicyMessage, ...customMessage };
+    const policyRequirement: string = policy.policyRequirement;
+    let message = policyMessage.unknownPolicy(property, { policyRequirement });
 
     Object.keys(policyName).forEach((name) => {
-      if (policyName[name] === policy.policyRequirement) {
+      if (policyName[name] === policyRequirement) {
         const params = policy.params || {};
         message = policyMessage[name](property, params);
       }
