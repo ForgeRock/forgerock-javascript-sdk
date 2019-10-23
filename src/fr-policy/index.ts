a701af06a7a4db2e0dddbd9cbca7fb1596f1f24b
Demetrio Urquidi <demetrio.urquidi@forgerock.com>
2019-10-23 15:21:16 -0500

    - params and return for docs - exports

--- src/fr-policy/index.ts
+++ src/fr-policy/index.ts
@@ -4,6 +4,14 @@ import { PolicyKey, policyMessage } from './enums';
 import { MessageCreator, ProcessedPropertyError } from './interfaces';
 
 abstract class FRPolicy {
+  /**
+   * Parses policy errors and generates human readable error messages.
+   *
+   * @param {Step} err The step containing the error.
+   * @param {MessageCreator} customMessage
+   * Extensible and overiable custom error messages for policy faiures.
+   * @return {ProcessedPropertyError[]} Array of objects contianing all processed policy errors.
+   */
   public static parseErrors(
     err: Partial<Step>,
     customMessage?: MessageCreator,
@@ -23,6 +31,15 @@ abstract class FRPolicy {
     return errors;
   }
 
+  /**
+   * Parses a failed policy and return a string array of error messages.
+   *
+   * @param {FailedPolicyRequirement} failePolicy The detail data of the failed policy.
+   * @param {MessageCreator} customMessage
+   * Extensible and overiable custom error messages for policy faiures.
+   * @return {string[]} Array of strings with all processed policy errors.
+   */
+
   public static parseFailedPolicyRequirement(
     failePolicy: FailedPolicyRequirement,
     customMessage?: MessageCreator,
@@ -36,6 +53,15 @@ abstract class FRPolicy {
     return errors;
   }
 
+  /**
+   * Parses a policy error into a human readable error message.
+   *
+   * @param {string} property The property with the policy failure.
+   * @param {PolicyRequirement} policy The policy failure data.
+   * @param {MessageCreator} customMessage
+   * Extensible and overiable custom error messages for policy faiures.
+   * @return {string} Human readable error message.
+   */
   public static parsePolicyRequirement(
     property: string,
     policy: PolicyRequirement,
@@ -44,14 +70,14 @@ abstract class FRPolicy {
     const policyRequirement = policy.policyRequirement;
     const params = policy.params ? { ...policy.params, policyRequirement } : { policyRequirement };
     const message = (
-      customMessage[policyRequirement]
-      || policyMessage[policyRequirement]
-      || policyMessage[PolicyKey.unknownPolicy]
-    )(property, params);
+      customMessage[policyRequirement] ||
+      policyMessage[policyRequirement] ||
+      policyMessage[PolicyKey.unknownPolicy]
+      )(property, params);
 
     return message;
   }
 }
 
 export default FRPolicy;
-export { PolicyKey };
+export { PolicyKey, MessageCreator, ProcessedPropertyError };
