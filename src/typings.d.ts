a7775955fe554b1cd7ca554a6eac343bd7044ed5
Jared Jensen <jared.jensen@forgerock.com>
2019-11-01 09:16:09 -0700

    Reorganized test and unnecessary typings/config

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,20 +1,3 @@
-declare module '*.html' {
-  const content: string;
-  export = content;
-}
-
-declare module '*.css' {
-  const content: string;
-  export = content;
-}
-
-declare module '*.scss' {
-  const content: string;
-  export = content;
-}
-
-declare module 'fake-indexeddb/auto';
-
 interface Window {
   PublicKeyCredential: any;
 }
