6fdb68b9670ff21fd324711a6912eab4756e6470
Justin Lowery <justin.lowery@forgerock.com>
2020-07-07 17:44:46 -0500

    Test (reg/2nd-factor): registration and second factor automation

---
+++ tests/e2e/suites/registration.test.ts
@@ -0,0 +1,31 @@
+import { setupAndGo } from '../utilities/setup-and-go';
+
+describe('Test basic registration flow', () => {
+  ['chromium', 'webkit', 'firefox'].forEach((browserType) => {
+    it(`should register user successfully and then log out with ${browserType}`, async (done) => {
+      const { browser, page } = await setupAndGo(browserType, 'registration/');
+
+      const messageArray = [];
+
+      page.on('console', (msg) => {
+        messageArray.push(msg.text());
+      });
+
+      await page.waitForSelector('.Test_Complete');
+
+      // Test assertions
+      expect(messageArray.includes('Prompt from UsernameCallback is Username')).toBe(true);
+      expect(messageArray.includes('Prompt from PasswordCallback is Password')).toBe(true);
+      expect(messageArray.includes('Prompt 1: First Name')).toBe(true);
+      expect(messageArray.includes('Prompt 2: Last Name')).toBe(true);
+      expect(messageArray.includes('Prompt 3: Email Address')).toBe(true);
+      expect(messageArray.includes('Prompt 1: Select a security question')).toBe(true);
+      expect(messageArray.includes(`Predefined Question1: What's your favorite color?`)).toBe(true);
+      expect(messageArray.includes('Terms version: 0.0')).toBe(true);
+      expect(messageArray.includes('Terms text: Lorem ipsum dolor sit amet ...')).toBe(true);
+
+      await browser.close();
+      done();
+    });
+  });
+});
