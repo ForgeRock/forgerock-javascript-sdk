287fc7dc98a733ba7d7f26ccf03e8a639eb07d9b
George Bafaloukas <george.bafaloukas@forgerock.com>
2021-05-04 15:15:15 +0100

    Removef createVerifier parameter

--- src/util/pkce.ts
+++ src/util/pkce.ts
@@ -23,11 +23,8 @@ abstract class PKCE {
   /**
    * Creates a random verifier.
    */
-  public static createVerifier(num?: number): string {
-    if (num) {
-      console.warn('Deprecation warning: the parameter for `createVerifier` will be removed in v3');
-    }
-    return this.createRandomString(num || 32);
+  public static createVerifier(): string {
+    return this.createRandomString(32);
   }
 
   /**
