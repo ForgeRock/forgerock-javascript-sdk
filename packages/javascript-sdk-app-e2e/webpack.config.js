edf13f571b667cfdac414481700f117f92a175a1
ryanbas21 <ryan.basmajian@forgerock.com>
2021-11-24 12:53:38 -0700

    feature(E2E): Run E2E through Nx
    In order to run e2e's through nx we need to define an 'app'. We don't
    have a clear 'app' per se but I split the 'e2e' app into its own
    'package'. This allows us to create a playwright e2e for the app itself.
    
    Hypothetically, we could create a lot of individual apps and have a
    potentially more affected build through nx. This was a big chunk of work
    and I deem that to be a long term goal if possible. This still utilizes
    webpack to run the tests.
    
    This PR is probably not done, I need to do more testing around the
    output folders to ensure its a stable and clean build/teardown as I
    think there may be some outlier with ts outputs happening

--- packages/javascript-sdk-app-e2e/webpack.config.js
+++ packages/javascript-sdk-app-e2e/webpack.config.js
@@ -1,8 +1,7 @@
 const { exec } = require('child_process');
 const CopyPlugin = require('copy-webpack-plugin');
-const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
+const path = require('path');
 
 const banner = `
 @forgerock/javascript-sdk
@@ -14,17 +13,18 @@ This software may be modified and distributed under the terms
 of the MIT license. See the LICENSE file for details.
 `;
 
-module.exports = (env) => {
-  const isDev = env.DEV === 'yes';
+module.exports = (config, context) => {
+  const isDev = context.configuration === 'development';
   const plugins = [
-    new webpack.WatchIgnorePlugin({ paths: [/bundles|docs|lib|lib\-esm|samples/] }),
+    ...config.plugins,
+    new webpack.WatchIgnorePlugin({ paths: [/bundles|docs|lib|lib-esm|samples/] }),
     new webpack.BannerPlugin({ banner }),
     new CopyPlugin({
       // Copy and rename non-built config files
       patterns: [
         {
-          from: './javascript-sdk-e2e/src/e2e/env.config.ts',
-          to: './javascript-sdk-e2e/src/e2e/server/env.config.copy.mjs',
+          from: path.resolve(__dirname, '../javascript-sdk-app-e2e/src/env.config.ts'),
+          to: path.resolve(__dirname, '../javascript-sdk-app-e2e/src/server/env.config.copy.mjs'),
           force: true,
           toType: 'file',
           // minimized with value true doesn't minimize; yup, you read that right
@@ -35,10 +35,10 @@ module.exports = (env) => {
     {
       apply: (compiler) => {
         // Copy built files
-        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+        compiler.hooks.afterEmit.tap('AfterEmitPlugin', () => {
           const cmds = [
-            'copyfiles -u 1 "./bundles/index.js*" ../javascript-sdk-e2e/src/e2e/app/',
-            'copyfiles -u 1 "./bundles/index.js*" ../../samples/_static/js/',
+            'npx copyfiles -u 1 "../javascript-sdk/bundles/index.js*" ../javascript-sdk-app/src/app/',
+            'npx copyfiles -u 1 "../javascript-sdk/bundles/index.js*" ../../samples/_static/js/',
           ];
           for (const cmd of cmds) {
             exec(cmd, (err, stdout, stderr) => {
@@ -57,8 +57,8 @@ module.exports = (env) => {
   ];
 
   return {
+    ...config,
     devtool: isDev ? 'inline-source-map' : 'source-map',
-    entry: path.resolve(__dirname, 'src/index.ts'),
     mode: isDev ? 'development' : 'production',
     module: {
       rules: [
@@ -76,25 +76,7 @@ module.exports = (env) => {
         },
       ],
     },
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          extractComments: false,
-        }),
-      ],
-    },
-    output: {
-      filename: 'index.js',
-      library: 'forgerock',
-      libraryTarget: 'umd',
-      path: path.resolve(__dirname, './bundles'),
-      umdNamedDefine: true,
-    },
     plugins,
-    resolve: {
-      extensions: ['.js', '.ts'],
-    },
     watch: isDev,
   };
 };
