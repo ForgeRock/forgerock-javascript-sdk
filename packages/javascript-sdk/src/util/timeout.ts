8f158f412f312580fed5309791bf39ba122cc630
Ryan Bas <ryan.basmajian@forgerock.com>
2022-02-15 11:25:44 -0700

    test(timeout.ts): Add test cases for the withTimeout function
    There was a zendesk ticket that came up about the withTimeout function
    so i've added test cases and may have discovered a weird microtask issue
    with promises being raced.

--- packages/javascript-sdk/src/util/timeout.ts
+++ packages/javascript-sdk/src/util/timeout.ts
@@ -17,12 +17,11 @@ import { DEFAULT_TIMEOUT } from '../config';
  */
 function withTimeout<T>(promise: Promise<T>, timeout: number = DEFAULT_TIMEOUT): Promise<T> {
   const effectiveTimeout = timeout || DEFAULT_TIMEOUT;
-  return Promise.race([
-    promise,
-    new Promise<T>((_, reject) =>
-      window.setTimeout(() => reject(new Error('Timeout')), effectiveTimeout),
-    ),
-  ]);
+  const timeoutP = new Promise<T>((_, reject) =>
+    window.setTimeout(() => reject(new Error('Timeout')), effectiveTimeout),
+  );
+
+  return Promise.race([promise, timeoutP]);
 }
 
 export { withTimeout };
