277f07d73e890454ab794e5c0a7fa4438d24a25c
Ryan Bas <ryan.basmajian@forgerock.com>
2022-04-20 12:59:32 -0600

    task(ci-refactor): Refactor CI
    Refactor of CI to fit new nx suggestions

--- packages/javascript-sdk/src/index.ts
+++ packages/javascript-sdk/src/index.ts
@@ -7,7 +7,7 @@
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
-
+// remove this after
 import Auth from './auth';
 import { CallbackType, ErrorCode } from './auth/enums';
 import { Callback, NameValue, PolicyRequirement, Step, StepDetail } from './auth/interfaces';
