0f4e04713e78d567a3a411bc0ab0e5671c1a589d
David Adams <david.adams@forgerock.com>
2022-08-19 16:10:43 +0100

    Implemented widget modal for login

--- samples/angular-todo/src/app/views/login/login.component.ts
+++ samples/angular-todo/src/app/views/login/login.component.ts
@@ -8,7 +8,11 @@
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+// @ts-ignore
+import Widget, { modal, journey } from 'forgerock-web-login-widget/modal';
+import { Router } from '@angular/router';
 
 /**
  * Used to show a login page
@@ -17,4 +21,35 @@ import { Component } from '@angular/core';
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const loginWidget = new Widget({
+      target: document.getElementById('login-widget'), // Any existing element from static HTML file
+      props: {
+        config: {
+          clientId: environment.WEB_OAUTH_CLIENT,
+          redirectUri: environment.APP_URL,
+          scope: 'openid profile email',
+          serverConfig: {
+            baseUrl: environment.AM_URL,
+            timeout: 30000, // 90000 or less
+          },
+          realmPath: environment.REALM_PATH,
+          tree: environment.JOURNEY_LOGIN,
+        },
+      },
+    });
+    modal.open();
+
+    journey.onSuccess((userData: any) => {
+      console.log(userData);
+      loginWidget.$destroy();
+      this.router.navigateByUrl('/');
+    });
+    journey.onFailure((error: any) => {
+      console.log(error);
+    });
+  }
+}
