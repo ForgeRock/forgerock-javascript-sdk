f831f0771abf1afaa2679ea190119c46dd5462c3
ryanbas21 <ryan.basmajian@forgerock.com>
2023-06-14 16:18:21 -0600

    feat: use changesets to better manage releases
    using changesets/changesets to manage releases in the monorepo.

--- samples/angular-todo/src/environments/environment.ts
+++ samples/angular-todo/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  AM_URL: 'https://openam-ryan-bas.forgeblocks.com/am',
-  REALM_PATH: 'alpha',
-  WEB_OAUTH_CLIENT: 'example-react-app',
-  JOURNEY_LOGIN: 'Login',
-  JOURNEY_REGISTER: 'Registration',
-  API_URL: 'http://localhost:9443',
-  production: 'development',
+   AM_URL: 'https://openam-ryan-bas.forgeblocks.com/am',
+   REALM_PATH: 'alpha',
+   WEB_OAUTH_CLIENT: 'example-react-app',
+   JOURNEY_LOGIN: 'Login',
+   JOURNEY_REGISTER: 'Registration',
+   API_URL: 'http://localhost:9443',
+   production: 'development'
 };
