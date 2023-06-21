e9649911d216ede20acf34f2f5ee33da8a2f31e2
ryanbas21 <ryan.basmajian@forgerock.com>
2023-06-20 19:39:23 -0600

    build: update repo to have ci use new parralell flow
    update ci to use new nx flow

--- samples/angular-todo/src/environments/environment.ts
+++ samples/angular-todo/src/environments/environment.ts
@@ -1,19 +1,9 @@
 export const environment = {
-<<<<<<< HEAD
-   AM_URL: 'https://openam-ryan-bas.forgeblocks.com/am',
-   REALM_PATH: 'alpha',
-   WEB_OAUTH_CLIENT: 'example-react-app',
-   JOURNEY_LOGIN: 'Login',
-   JOURNEY_REGISTER: 'Registration',
-   API_URL: 'http://localhost:9443',
-   production: 'development'
-=======
-  AM_URL: 'https://openam-crbrl-01.forgeblocks.com/am/',
-  REALM_PATH: 'alpha',
-  WEB_OAUTH_CLIENT: 'WebOAuthClient',
-  JOURNEY_LOGIN: 'Login',
-  JOURNEY_REGISTER: 'Register',
-  API_URL: 'http://localhost:9443',
-  production: 'development',
->>>>>>> 72d2d34 (fix(javascript-sdk): update-ci-release)
+  AM_URL: '',
+  REALM_PATH: '',
+  WEB_OAUTH_CLIENT: '',
+  JOURNEY_LOGIN: '',
+  JOURNEY_REGISTER: '',
+  API_URL: '',
+  production: '',
 };
