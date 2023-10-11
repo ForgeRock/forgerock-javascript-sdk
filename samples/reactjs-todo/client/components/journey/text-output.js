a094e1b22c350ac10a0f3e3f778ce0313c101392
juanManuel05 <juangarmendia2000@hotmail.com>
2023-10-11 16:02:40 -0300

    feat(reactjs-todo): add WebAuthn support for react todo sample app

---
+++ samples/reactjs-todo/client/components/journey/text-output.js
@@ -0,0 +1,23 @@
+/*
+ * forgerock-sample-web-react
+ *
+ * textOutput.js
+ *
+ * Copyright (c) 2021 ForgeRock. All rights reserved.
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+/**
+ * @function TextOutput- React component used for displaying text Outputcallback
+ * @param {Object} props - React props object passed from parent
+ * @returns {Object} - React component object
+ */
+
+/** 
+ component added to render purposes, at this point this callback renders no element
+*/
+
+// eslint-disable-next-line no-unused-vars
+export default function TextOutput({ inputName }) {
+  return null;
+}
