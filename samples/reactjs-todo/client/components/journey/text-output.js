e46fc16e2132ffda572525b597aaddf497af38f1
Justin Lowery <justin.lowery@forgerock.com>
2023-10-19 11:04:05 -0500

    chore(reactjs-todo): fix journey-state with multi-step failure

--- samples/reactjs-todo/client/components/journey/text-output.js
+++ samples/reactjs-todo/client/components/journey/text-output.js
@@ -7,6 +7,7 @@
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
+import React from 'react';
 /**
  * @function TextOutput- React component used for displaying text Outputcallback
  * @param {Object} props - React props object passed from parent
@@ -17,7 +18,7 @@
  component added to render purposes, at this point this callback renders no element
 */
 
-// eslint-disable-next-line no-unused-vars
-export default function TextOutput({ inputName }) {
-  return null;
+export default function TextOutput({ callback }) {
+  const existingValue = callback.getMessage();
+  return <p> {existingValue} </p>;
 }
