4acca848a11bccea3f7ddc022a648c6e40ca936f
ryanbas21 <ryan.basmajian@forgerock.com>
2023-10-20 12:53:42 -0600

    fix(mock-api): fix-introspect-call
    the api changed and no longer allows the introspect call to pass the query param token, it needs it
    in the post body

--- samples/angular-todo/src/environments/environment.ts
+++ samples/angular-todo/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  AM_URL: 'https://openam-sdks.forgeblocks.com/am/',
-  REALM_PATH: 'alpha',
-  WEB_OAUTH_CLIENT: 'WebOAuthClient',
-  JOURNEY_LOGIN: 'Login',
-  JOURNEY_REGISTER: 'Registration',
-  API_URL: 'https://auth.example.com:9443',
-  production: 'development',
+   AM_URL: 'https://openam-crbrl-01.forgeblocks.com/am/',
+   REALM_PATH: 'alpha',
+   WEB_OAUTH_CLIENT: 'WebOAuthClient',
+   JOURNEY_LOGIN: 'UsernamelessWebAuthn',
+   JOURNEY_REGISTER: 'Registration',
+   API_URL: 'http://localhost:9443',
+   production: 'development'
 };
