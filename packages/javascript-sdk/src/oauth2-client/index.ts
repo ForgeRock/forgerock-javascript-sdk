b4ba91ec8247d3fee423275e45b5acd0c9859829
Justin Lowery <justin.lowery@forgerock.com>
2023-11-01 10:51:11 -0500

    feat(javascript-sdk): include raw response to OAuth token return
    - add raw response to returned value in OAuth2Client
    - this raw response is the original serialized JSON body
    - this new property will be persisted with the tokens in WebStorage

--- packages/javascript-sdk/src/oauth2-client/index.ts
+++ packages/javascript-sdk/src/oauth2-client/index.ts
@@ -160,6 +160,7 @@ abstract class OAuth2Client {
     };
 
     const response = await this.request('accessToken', undefined, false, init, options);
+    const responseClone = response.clone();
     const responseBody = await this.getBody<unknown>(response);
 
     if (response.status !== 200) {
@@ -185,6 +186,7 @@ abstract class OAuth2Client {
       idToken: responseObject.id_token,
       refreshToken: responseObject.refresh_token,
       tokenExpiry: tokenExpiry,
+      rawResponse: await responseClone.text(),
     };
   }
 
