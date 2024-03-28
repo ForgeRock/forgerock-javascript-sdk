1a05074a55788243e3d33f837205654f03a4cf67
ryanbas21 <ryan.basmajian@forgerock.com>
2024-03-28 14:58:13 -0600

    fix(javascript-sdk): circular-dep
    remove circular-dep-on-config

--- packages/javascript-sdk/src/util/timeout.ts
+++ packages/javascript-sdk/src/util/timeout.ts
@@ -8,7 +8,7 @@
  * of the MIT license. See the LICENSE file for details.
  */
 
-import { DEFAULT_TIMEOUT } from '../config';
+import { DEFAULT_TIMEOUT } from '../config/constants';
 
 /**
  * @module
