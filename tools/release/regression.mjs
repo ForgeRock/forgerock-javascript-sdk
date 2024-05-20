ad84064e712ddf1c2ff4f1cbe37d698316083c57
ryanbas21 <ryan.basmajian@forgerock.com>
2024-05-20 09:17:56 -0600

    build: update-nx-19
    update deps to nx 19

--- tools/release/regression.mjs
+++ tools/release/regression.mjs
@@ -1,21 +1,18 @@
 import * as console from 'console';
-import * as process from 'process';
 
 import { releasePublish, releaseVersion } from 'nx/release/index.js';
 import { execSync } from 'child_process';
-
-const { env: { CI = false } = {} } = process;
 
 execSync('npx npm-cli-login -u test -p 1234 -e test@domain.test -r https://npm.petrov.ca');
 
 const { workspaceVersion, projectsVersionData } = await releaseVersion({
   specifier: 'prerelease', // this add the extra .0/1/2/3/* to the version
-  preid: CI === true ? 'dev-ci' : 'beta', // for sanity check - set preid to beta
+  preid: 'regression',
   dryRun: false,
   verbose: false,
   stageChanges: false,
-  gitCommit: CI === true,
-  gitTag: CI === true,
+  gitCommit: true,
+  gitTag: true,
 });
 
 execSync('npx nx run-many -t build --projects=tag:scope:package');
@@ -25,7 +22,7 @@ console.log('Workspace version (root package.json) should match: ', workspaceVer
 
 // The returned number value from releasePublish will be zero if all projects are published successfully, non-zero if not
 const publishStatus = await releasePublish({
-  tag: CI === true ? 'dev-ci' : 'beta', // sanity - lets at least tag as beta in case somehow it publishes to npm.
+  tag: 'regression',
   dryRun: false,
   verbose: false,
   generatorOptionsOverrides: {
