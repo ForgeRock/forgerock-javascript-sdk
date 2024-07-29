82a87be1d50f7fbd384c6cee5a9669930408c7ab
ryanbas21 <ryanbas21@gmail.com>
2024-07-29 13:11:34 -0600

    feat: support-metadata-marketplace-protect
    adds a `parse` method to parse a metadata callback, that
    can return 3 types of callbacks. Metadata - data, initialize
    and evaluation nodes from PingProtect

--- packages/javascript-sdk/src/fr-auth/callbacks/metadata-callback.ts
+++ packages/javascript-sdk/src/fr-auth/callbacks/metadata-callback.ts
@@ -10,6 +10,29 @@
 
 import FRCallback from '.';
 import type { Callback } from '../../auth/interfaces';
+import PingOneProtectEvaluationCallback from './ping-protect-evaluation-callback';
+import PingOneProtectInitializeCallback from './ping-protect-initialize-callback';
+
+type Metadata =
+  | {
+      _type: 'PingOneProtect';
+      _action: 'protect_risk_evaluation';
+      envId: string;
+      pauseBehavioralData: string;
+    }
+  | {
+      _type: 'PingOneProtect';
+      _action: 'protect_initialize';
+      consoleLogEnabled: boolean;
+      deviceAttributesToIgnore: string[];
+      customHost: string;
+      lazyMetadata: boolean;
+      behavioralDataCollection: boolean;
+      deviceKeyRsyncIntervals: number;
+      enableTrust: boolean;
+      disableTags: boolean;
+      disableHub: boolean;
+    };
 
 /**
  * Represents a callback used to deliver and collect miscellaneous data.
@@ -22,6 +45,44 @@ class MetadataCallback extends FRCallback {
     super(payload);
   }
 
+  /**
+   * Will parse the given callback from initialization
+   * and either return the metadata provided
+   * or will try to return the ping protect initialization
+   * or a protect evaluation callback
+   */
+  parse(index: number) {
+    try {
+      const payload = this.payload.output[index].value as Metadata;
+      /**
+       * parse logic begins with an if we have a _type
+       * then we should continue to the next logical step
+       *
+       */
+      if (payload._type && payload._type === 'PingOneProtect') {
+        /**
+         * Next we need to evaluate if the metadata
+         * is protect related
+         */
+        if (payload._action && payload._action === 'protect_initialize') {
+          /***
+           * Because we have determined this is a protect callback, we should convert it
+           * based on the type of callback it is
+           ***/
+          return new PingOneProtectInitializeCallback(this.payload);
+        } else if (payload._action === 'protect_risk_evaluation') {
+          return new PingOneProtectEvaluationCallback(this.payload);
+        }
+      }
+      /**
+       * if we don't havea  protect initialize or evaluation
+       * type we just return the metadata
+       */
+      return this.getData();
+    } catch (err) {
+      throw new Error(`failed to parse callback, ${err}`);
+    }
+  }
   /**
    * Gets the callback's data.
    */
