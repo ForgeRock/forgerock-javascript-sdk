d3a1f5454f3419f4da58e137a50bf0afff2ab8ad
ryanbas21 <ryanbas21@gmail.com>
2024-12-16 15:20:39 -0700

    chore: remove-token-vault

--- packages/token-vault/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { interceptor as interceptorModule } from './lib/worker/index.js';
-import { client as clientModule } from './lib/client.js';
-import { proxy as proxyModule } from './lib/proxy.js';
-
-export const client = clientModule;
-export const interceptor = interceptorModule;
-export const proxy = proxyModule;
